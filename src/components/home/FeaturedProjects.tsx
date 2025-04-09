2f947b276ccc869fea3e30a91e3e7405af1737ef
likshan-lahiru <lahiru212001@gmail.com>
2025-04-09 14:42:51 +0530

    1. update project card 2. contact form map section 3. and change about section image

--- src/components/home/FeaturedProjects.tsx
+++ src/components/home/FeaturedProjects.tsx
@@ -1,108 +1,102 @@
-import  { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { motion, AnimatePresence } from 'framer-motion'
-import { ArrowRightIcon } from 'lucide-react'
-import SectionTitle from '../ui/SectionTitle'
-
+import  { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import { ArrowRightIcon } from "lucide-react";
+import SectionTitle from "../ui/SectionTitle";
+import Button from "../ui/Button";
 const projects = [
   {
     id: 1,
     contents: [
       {
-        title: 'Ace Healthcare Derma',
-        category: 'Commercial Building',
+        title: "Ace Healthcare Derma",
+        category: "Commercial Building",
         description:
-            'COSMETIC AND DERMA FORMULATION PLANT FOR ACE HEALTHCARE (PVT) LTD, HORANA',
+            "Cosmetic and Derma Formulation Plant for Ace Healthcare (Pvt) Ltd, Horana",
       },
       {
-        title: 'Amrith',
-        category: 'Residential Building',
+        title: "Amrith",
+        category: "Residential Building",
         description:
-            'RESIDENCE FOR AMRITH DE SOYSA AND SHIHANI DE SOYSA AT PARK ROAD, COLOMBO',
+            "Residence for Amrith De Soysa and Shihani De Soysa at Park Road, Colombo",
       },
       {
-        title: 'BOC Kandy',
-        category: 'Commercial Building',
+        title: "Bank of Ceylon Kandy",
+        category: "Commercial Building",
         description:
-            'RENNOVATION / MODIFICATION OF BANK OF CEYLON HOLIDAY HOME AT KANDY',
+            "Rennovation / Modification of Bank of Ceylon Holiday Home at Kandy",
       },
     ],
     images: [
-      'https://images.unsplash.com/photo-1486325212027-8081e485255e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
-      'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+      "/src/assets/images/Horana2.jpg",
+      "/src/assets/images/Armith.jpg",
+      "/src/assets/images/BOCKandy.jpg",
     ],
-    link: '/projects/parkview-plaza',
+    link: "/projects/parkview-plaza",
   },
   {
     id: 2,
     contents: [
       {
-        title: 'BOC Trinco',
-        category: 'Commercial Building',
-        description:
-            'BUILDING FOR BANK OF CEYLON CITY BRANCH AT TRINCOMALEE',
+        title: "Bank of Ceylon Trinco",
+        category: "Commercial Building",
+        description: "Building for Bank of Ceylon City Branch at Trincomalee",
       },
       {
-        title: 'CEYLON COLLEGE OF PHYSICIANS BUILDING',
-        category: 'Commercial Building',
-        description:
-            'RENOVATION OF CEYLON COLLEGE OF PHYSICIANS BUILDING',
+        title: "Renovation of Ceylon College",
+        category: "Commercial Building",
+        description: "Renovation of Ceylon College of Physicians building",
       },
       {
-        title: 'Colombo Urban Regeneration',
-        category: 'Urban Ruler and Regional Development',
-        description:
-            'COLOMBO URBAN REGENERATION PROJECT, SRI LANKA',
+        title: "Colombo Urban Regeneration",
+        category: "Urban Ruler and Regional Development",
+        description: "Colombo Urban Regeneration Project, Sri Lanka",
       },
     ],
     images: [
-
-      'https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1453&q=80',
-      'https://images.unsplash.com/photo-1513828583688-c52646db42da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-      'https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1453&q=80',
+      "/src/assets/images/BOCTrinco.jpg",
+      "/src/assets/images/ccp.jpg",
+      "/src/assets/images/ColomboUrbanRegeneration.jpg",
     ],
-    link: '/projects/riverside-bridge',
+    link: "/projects/riverside-bridge",
   },
   {
     id: 3,
     contents: [
       {
-        title: 'CTB Fuel',
-        category: 'Urban Ruler and Regional Development.',
+        title: "Ceypetco Fuel Station",
+        category: "Urban Ruler and Regional Development.",
         description:
-            'FUEL STATION FOR SRI LANKA TRANSPORT BOARD AT BASELINE ROAD, ORUGODAWATTA',
+            "Fuel Station for Sri Lanka Transport Board at Baseline Road, Orugodawatta",
       },
       {
-        title: 'Dr. Sanjeewa',
-        category: 'Residential Building',
-        description:
-            'RESIDENCE FOR DR. RANIL SANJEEWA AND DR. GAYATHRI KANCHANA',
+        title: "Dr. Sanjeewa",
+        category: "Residential Building",
+        description: "Residence for DR.Ranil Sanjeewa and DR.Gayathri Kanchana",
       },
       {
-        title: 'Galle Udugama',
-        category: 'Road & Infastructure',
-        description:
-            'WIDENING AND IMPROVEMENTS TO GALLE-UDUGAMA ROAD',
+        title: "Widening and Improvements",
+        category: "Road & Infastructure",
+        description: "Widening and Improvements to Galle-Udugama road",
       },
     ],
     images: [
-      'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80',
-      'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-      'https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1453&q=80',
+      "/src/assets/images/Fuel.jpg",
+      "/src/assets/images/Sanjeewa.jpg",
+      "/src/assets/images/galle-udugama.jpg",
     ],
-    link: '/projects/green-valley-residences',
+    link: "/projects/green-valley-residences",
   },
-]
+];
 const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => {
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % project.images.length)
-    }, 5000)
-    return () => clearInterval(interval)
-  }, [project.images.length])
-  const content = project.contents[currentIndex]
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % project.images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [project.images.length]);
+  const content = project.contents[currentIndex];
   return (
       <motion.div
           whileHover={{
@@ -116,7 +110,7 @@ const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => {
           }}
           className="group relative h-[400px] md:h-[450px] lg:h-[400px] w-full overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-all duration-500"
       >
-        <motion.div className="absolute inset-0 bg-black opacity-40 group-hover:opacity-70 transition-opacity duration-500" />
+        <motion.div className="absolute inset-0 transition-opacity duration-500 bg-black opacity-40 group-hover:opacity-70" />
         <AnimatePresence mode="wait">
           <motion.img
               key={currentIndex}
@@ -134,10 +128,10 @@ const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => {
               transition={{
                 duration: 0.7,
               }}
-              className="absolute inset-0 h-full w-full object-cover object-center transition-transform duration-700 group-hover:scale-110"
+              className="absolute inset-0 object-cover object-center w-full h-full transition-transform duration-700 group-hover:scale-110"
           />
         </AnimatePresence>
-        <motion.div className="absolute inset-0 p-8 flex flex-col justify-between z-10">
+        <motion.div className="absolute inset-0 z-10 flex flex-col justify-between p-8">
           <AnimatePresence mode="wait">
             <motion.div
                 key={currentIndex}
@@ -158,10 +152,10 @@ const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => {
                   duration: 0.5,
                 }}
             >
-              <motion.span className="inline-block text-sm font-medium bg-blue-900/80 px-3 py-1 rounded-full text-white">
+              <motion.span className="inline-block px-3 py-1 text-sm font-medium text-white rounded-full bg-blue-900/80">
                 {content.category}
               </motion.span>
-              <motion.h3 className="text-3xl md:text-4xl font-bold text-white">
+              <motion.h3 className="text-3xl font-bold text-white md:text-4xl">
                 {content.title}
               </motion.h3>
             </motion.div>
@@ -187,48 +181,48 @@ const ProjectCard = ({ project }: { project: (typeof projects)[0] }) => {
                   delay: 0.1,
                 }}
             >
-              <motion.p className="text-white text-lg leading-relaxed">
+              <motion.p className="text-lg leading-relaxed text-white">
                 {content.description}
               </motion.p>
-              <motion.div className="flex items-center text-yellow-400 font-semibold group/link">
-                <Link
+              <motion.div className="flex items-center font-semibold text-yellow-400 group/link">
+         {/*       <Link
                     to={project.link}
                     className="flex items-center hover:text-yellow-300"
                 >
                   Read More
                   <ArrowRightIcon
                       size={20}
-                      className="ml-2 transform transition-transform duration-300 group-hover/link:translate-x-1"
+                      className="ml-2 transition-transform duration-300 transform group-hover/link:translate-x-1"
                   />
-                </Link>
+                </Link>*/}
               </motion.div>
             </motion.div>
           </AnimatePresence>
         </motion.div>
       </motion.div>
-  )
-}
+  );
+};
 const FeaturedProjects = () => {
   return (
       <section className="py-16 bg-white">
-        <div className="container mx-auto px-4">
+        <div className="container px-4 mx-auto">
           <SectionTitle
               title="Featured Projects"
               subtitle="Explore some of our most impactful work across various sectors."
               centered={true}
           />
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 md:gap-8">
             {projects.map((project) => (
                 <ProjectCard key={project.id} project={project} />
             ))}
           </div>
           <div className="mt-12 text-center">
-      {/*      <Button to="/projects" variant="secondary">
+{/*            <Button to="/projects" variant="secondary">
               View All Projects
             </Button>*/}
           </div>
         </div>
       </section>
-  )
-}
-export default FeaturedProjects
+  );
+};
+export default FeaturedProjects;
