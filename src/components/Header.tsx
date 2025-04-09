9440bd9050d44445896bc45e70ac96485e934cc7
likshan-lahiru <lahiru212001@gmail.com>
2025-04-09 09:44:45 +0530

    create new dev branch

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+  return <div className="py-8">
+      <div className="flex items-center justify-center">
+        <h2 className="text-3xl font-bold text-white">3G Consultants</h2>
+      </div>
+    </div>;
+};
+export default Header;
