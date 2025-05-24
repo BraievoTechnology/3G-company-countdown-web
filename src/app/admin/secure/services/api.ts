9a0f1234aa28c1322c0937d958985f6db267c580
likshan-lahiru <lahiru212001@gmail.com>
2025-05-25 00:33:24 +0530

    Refactor date handling and update API configurations.
    Removed unnecessary date object conversions and ensured better consistency for start_date, end_date, and budget data handling. Updated console logs for debugging and adjusted API base URL comment to reflect usage in NEXT projects.

--- src/app/admin/secure/services/api.ts
+++ src/app/admin/secure/services/api.ts
@@ -10,7 +10,7 @@
 /*
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -21,7 +21,7 @@ export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // for Vite projects
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // for NEXT projects
   headers: {
     "Content-Type": "application/json",
   },
